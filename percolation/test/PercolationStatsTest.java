ebc7153922bac2fcf621dc636392d432909e43ba
akyidrian <akyidrian@gmail.com>
2017-04-26 15:38:58 +1200

    Forgot to commit percolation test. Adding it now.

---
+++ percolation/test/PercolationStatsTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Aydin on 26/03/2017.
+ */
+class PercolationStatsTest {
+    @Test
+    void mean() {
+    }
+
+    @Test
+    void stddev() {
+    }
+
+    @Test
+    void confidenceLo() {
+    }
+
+    @Test
+    void confidenceHi() {
+    }
+
+}
